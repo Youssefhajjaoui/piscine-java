c8414e7e4c96283279bce0ff45f7baab56ecd68e
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-11 16:09:42 +0100

    concat v3

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -2,6 +2,14 @@
 public class StringConcat {
 
     public static String concat(String s1, String s2) {
+        if (s2 == null && s1 == null) {
+            return null;
+        } else if (s1 == null) {
+            return s2;
+        } else if (s2 == null) {
+            return s1;
+        }
+
         return s1 + s2;
     }
 }
