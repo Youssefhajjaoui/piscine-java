b3be4436316c1c6e63c66af7d06d7837d0725663
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-14 00:10:46 +0100

    Plannet Mass

---
+++ StarMass/Galaxy.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.Map;
+
+public class Galaxy {
+    private List<CelestialObject> celestialObjects;
+
+    public Galaxy() {
+        this.celestialObjects = new java.util.ArrayList<CelestialObject>();
+    }
+
+    public List<CelestialObject> getCelestialObjects() {
+        return this.celestialObjects;
+    }
+
+    public void addCelestialObject(CelestialObject other) {
+        this.celestialObjects.add(other);
+    }
+
+    public Map<String, Integer> computeMassRepartition() {
+        Map<String, Integer> result = new java.util.HashMap<>();
+        result.put("Star", 0);
+        result.put("Planet", 0);
+        result.put("Other", 0);
+
+        for (CelestialObject obj : this.celestialObjects) {
+            String type;
+            if (obj instanceof Star) {
+                type = "Star";
+            } else if (obj instanceof Planet) {
+                type = "Planet";
+            } else {
+                type = "Other";
+            }
+            result.put(type, result.get(type) + obj.getMass());
+        }
+
+        return result;
+    }
+}
---
+++ StarMass/Planet.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+
+public class Planet extends CelestialObject {
+    private Star centerStar;
+
+    public Planet() {
+        super();
+        this.centerStar = new Star();
+    }
+
+    public Planet(String name, double x, double y, double z, Star centerStar, Integer mass) {
+        super();
+        this.name = name;
+        this.centerStar = centerStar;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.mass = mass;
+    }
+
+    public Star getCenterStar() {
+        return this.centerStar;
+    }
+
+    public void setCenterStar(Star other) {
+        this.centerStar = other;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.x, this.y, this.z, this.name, this.centerStar);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s circles around %s at the %.3f AU", this.name, this.centerStar.name,
+                CelestialObject.getDistanceBetween(this, this.getCenterStar()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        super.equals((CelestialObject) other);
+        Planet obj = (Planet) other;
+        return this.x == obj.x && this.y == obj.y && this.z == obj.z && this.name.equals(obj.name)
+                && this.centerStar.getMagnitude() == obj.centerStar.getMagnitude();
+    }
+}
+
+// getCenterStar and setCenterStar
