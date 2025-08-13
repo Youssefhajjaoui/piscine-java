f373c8e5b4dac7465b0412bbc208aa957971cc37
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-13 18:30:44 +0100

    StarStatic

--- StarConstructors/CelestialObject.java
+++ StarConstructors/CelestialObject.java
@@ -14,38 +14,6 @@ public class CelestialObject {
         this.z = z;
         this.name = name;
     }
-
-    public double getX() {
-        return this.x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return this.y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public double getZ() {
-        return this.z;
-    }
-
-    public void setZ(double z) {
-        this.z = z;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
 // getX and setX for the x property
