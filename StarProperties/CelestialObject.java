cf5883eb7755bdc4e817b21ccbf2f11bd6fe70e2
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-13 15:21:58 +0100

    StarProperties

---
+++ StarProperties/CelestialObject.java
@@ -0,0 +1,10 @@
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+    public static void main(String[] args) {
+
+    }
+}
