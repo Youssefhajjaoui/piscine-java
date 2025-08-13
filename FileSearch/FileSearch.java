e4dd5a82dfd416427b77a4c1ec048d20ef490437
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-13 15:09:11 +0100

    file searsh

---
+++ FileSearch/FileSearch.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
+public class FileSearch {
+    public static String searchFile(String fileName) {
+        Path document = Path.of("documents");
+        try {
+            Optional<Path> found = Files.walk(document)
+                    .filter(Files::isRegularFile)
+                    .filter(p -> p.getFileName().toString().equals(fileName))
+                    .findFirst();
+
+            return found.map(Path::toString)
+                    .orElse("File not found");
+        } catch (IOException e) {
+            return e.toString();
+        }
+    }
+}
