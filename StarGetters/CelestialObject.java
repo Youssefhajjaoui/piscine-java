20f2fdd3e40127dcf0d1d84494b5bbab5608c31b
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-13 18:09:35 +0100

    StarConstructors

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,57 @@
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+    public CelestialObject() {
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+// getX and setX for the x property
+
+// getY and setY for the y property
+
+// getZ and setZ for the z property
+
+// getName and setName for the name property
