0d3a3cecfa55924f9f843a946bbcdfc3e60fd40a
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-14 00:12:32 +0100

    Plannet Mass

--- StarMass/Star.java
+++ StarMass/Star.java
@@ -17,6 +17,15 @@ public class Star extends CelestialObject {
         // this.mass = mass;
     }
 
+    public Star(String name, double x, double y, double z, double magnitude, Integer mass) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+        this.magnitude = magnitude;
+        this.mass = mass;
+    }
+
     public double getMagnitude() {
         return this.magnitude;
     }
