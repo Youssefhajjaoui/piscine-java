8c38d53c9f0645dd2d885c903ebca92c280f5448
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-13 14:30:40 +0100

    Capitalize

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -1,12 +1,25 @@
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Capitalize {
     public static void capitalize(String[] args) throws IOException {
-        Path p = Paths.get(args[0]);
-        byte[] bytes = Files.readAllBytes(p);
-        Files.write(Path.of(args[1]), bytes);
+        Path inputPath = Paths.get(args[0]);
+        String[] words = Files.readString(inputPath)
+                .trim()
+                .split("\\s+");
+
+        StringBuilder result = new StringBuilder();
+        for (String name : words) {
+            if (!name.isEmpty()) {
+                result.append(Character.toUpperCase(name.charAt(0)))
+                        .append(name.substring(1).toLowerCase())
+                        .append(" ");
+            }
+        }
+
+        // Write to output file
+        Files.write(Paths.get(args[1]), result.toString().trim().getBytes());
     }
 }
