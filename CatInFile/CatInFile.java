06bce849d8a2ba52991c3abf656e577ed1d7859a
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-13 13:25:04 +0100

    Catinfile

--- CatInFile/CatInFile.java
+++ CatInFile/CatInFile.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class CatInFile {
     public static void cat(String[] args) throws IOException {
-        InputStream input = System.in;
-        byte[] b = input.readAllBytes();
+        byte[] b = System.in.readAllBytes();
         Files.write(Path.of(args[0]), b);
     }
 }
