1ed0b477024479fe4853101bb321c72e50a0c255
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-13 13:05:31 +0100

    Cat

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -7,16 +7,8 @@ public class Cat {
     public static void cat(String[] args) throws IOException {
         for (String path : args) {
             Path p = Paths.get(path);
-            try (BufferedReader reader = Files.newBufferedReader(p)) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
-            } catch (IOException e) {
-                System.err.println("Error: " + e.getMessage());
-                System.exit(1);
-            }
+            byte[] bytes = Files.readAllBytes(p);
+            System.out.print(bytes);
         }
-
     }
 }
