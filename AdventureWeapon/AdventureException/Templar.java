191e3611da5364ebec10400b9f6bbf36934d82e1
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-18 21:59:01 +0100

    Exception 3

---
+++ AdventureWeapon/AdventureException/Templar.java
@@ -0,0 +1,46 @@
+public class Templar extends Character implements Tank , Healer{
+	private final int healCapacity;
+	private final int shield;
+
+	public Templar(String name , int maxHealth , int HealCapacity , int shield){
+		super(name , maxHealth);
+		this.healCapacity = HealCapacity;
+		this.shield = shield;
+	}
+
+	@Override
+	public int getHealCapacity(){
+		return this.healCapacity;
+	}
+
+	@Override
+	public void heal(Character other){
+		other.addCurrentHealth(this.healCapacity);
+	}
+
+	@Override
+	public int getShield(){
+		return this.shield; 
+	}
+
+	@Override
+	public String toString(){
+		if(this.getCurrentHealth() != 0){
+			return String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d." , this.getName() , this.getCurrentHealth(), this.healCapacity , this.shield);
+		}else{
+			return String.format("%s has been beaten, even with its %d shield. So bad, it could heal %d HP." , this.getName(), this.shield ,this.healCapacity);
+		}
+	}
+
+	@Override
+	public void attack(Character other){
+		other.takeDamage(6);
+		this.heal(this);
+	}
+
+	@Override
+	public void takeDamage(int damage){
+		this.setCurrentHealth(this.getCurrentHealth() - (damage - shield));
+	}
+
+}
