87f804b98c63aea7428db1f6d46ec527f5781165
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-18 21:55:06 +0100

    Exception 3

---
+++ AdventureException/DeadCharacterException.java
@@ -0,0 +1,13 @@
+public class DeadCharacterException extends Exception {
+    private Character agent;
+
+    public DeadCharacterException(Character agent) {
+        this.agent = agent;
+    }
+
+    public String getMessage(){
+        String className = agent.getClass().getSimpleName().toLowerCase();
+        String name = agent.getName();
+        return "The " + className + " " + name + " is dead.";
+    }
+}
