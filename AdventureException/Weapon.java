4e9168c8f5fd5ccff45c258ab59bd93f6a4ea8f1
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-18 22:01:38 +0100

    Exception 3

---
+++ AdventureException/Weapon.java
@@ -0,0 +1,23 @@
+public class Weapon {
+    private final String name;
+    private final int damage;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public Weapon(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s deals %d damages", this.name, this.damage);
+    }
+
+}
