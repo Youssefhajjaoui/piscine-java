ecb1369b6350ea3596351e3b84a4740e88d3aaaa
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-18 21:21:16 +0100

    weapon

---
+++ AdventureWeapon/Monster.java
@@ -0,0 +1,34 @@
+public class Monster extends Character {
+
+	public Monster(String name, int maxHealth, Weapon w) {
+		super(name, maxHealth, w);
+	}
+
+	public Monster(String name, int maxHealth) {
+		super(name, maxHealth);
+	}
+
+	@Override
+	public String toString() {
+		if (this.getCurrentHealth() == 0) {
+			return String.format("%s is a monster and is dead", this.getName());
+		} else {
+			return String.format("%s is a monster with %d HP", this.getName(), this.getCurrentHealth());
+		}
+	}
+
+	@Override
+	public void attack(Character other) {
+		if (other.weapon != null) {
+			other.takeDamage(this.weapon.getDamage());
+
+		} else {
+			other.takeDamage(7);
+		}
+	}
+
+	@Override
+	public void takeDamage(int damage) {
+		this.setCurrentHealth(this.getCurrentHealth() - (int) Math.floor(damage * 0.8));
+	}
+}
---
+++ AdventureWeapon/Sorcerer.java
@@ -0,0 +1,51 @@
+public class Sorcerer extends Character implements Healer {
+	private final int healCapacity;
+
+	public Sorcerer(String name, int maxHealth, int capacity, Weapon w) {
+		super(name, maxHealth, w);
+		this.healCapacity = capacity;
+	}
+
+	public Sorcerer(String name, int maxHealth, int capacity) {
+		super(name, maxHealth);
+		this.healCapacity = capacity;
+	}
+
+	@Override
+	public int getHealCapacity() {
+		return this.healCapacity;
+	}
+
+	@Override
+	public void heal(Character other) {
+		other.addCurrentHealth(healCapacity);
+	}
+
+	@Override
+	public String toString() {
+		if (this.getCurrentHealth() != 0) {
+			return String.format("%s is a sorcerer with %d HP. It can heal %d HP.", this.getName(),
+					this.getCurrentHealth(), this.healCapacity);
+		} else {
+			return String.format("%s is a dead sorcerer. So bad, it could heal %d HP.", this.getName(),
+					this.healCapacity);
+		}
+	}
+
+	@Override
+	public void attack(Character other) {
+		this.heal(this);
+		if (other.weapon != null) {
+			other.takeDamage(this.weapon.getDamage());
+
+		} else {
+			other.takeDamage(10);
+		}
+	}
+
+	@Override
+	public void takeDamage(int damage) {
+		this.setCurrentHealth(this.getCurrentHealth() - damage);
+	}
+
+}
