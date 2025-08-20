654e7c9bd5788ae273841a89aad9a47aa8bda3e9
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-20 18:15:49 +0100

    Set equals

--- SetEquals/SetEquals.java
+++ SetEquals/SetEquals.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 public class SetEquals{
    public static boolean areSetsEqual(Set<String> set1, Set<String> set2) {
         if (set1 == null && set2 != null || set1 != null && set2 == null){
