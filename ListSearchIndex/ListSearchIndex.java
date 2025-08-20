3defa9791dd734aca1fc1361d396b62e6a56bb2b
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-20 16:59:03 +0100

    list seaarsh

---
+++ ListSearchIndex/ListSearchIndex.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSearchIndex{
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+        Integer num = null;
+	for (int i = 0 ; i < list.size() ; i++){
+		if (list.get(i).equals(value)){
+			num = i ; 
+		}
+	}
+	return num;
+    }
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+	        for(int i = 0; i<list.size() ; i++){
+			if( list.get(i).equals(value)){
+				return i;
+			}
+		}
+		return null;
+    }
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        List<Integer> list1 = new ArrayList<Integer>();
+	for(int i=0 ; i < list.size() ; i++){
+		if (list.get(i).equals(value)){
+			list1.add(i);
+		}
+	}
+	return list1;
+    }
+
+}
