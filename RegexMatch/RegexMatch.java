4f3cc38a1aef096569620dd9d73e43d666723053
yhajjaou <youssefhajjaoui67@gmail.com>
2025-08-25 17:58:00 +0100

    fornmatedate

---
+++ RegexMatch/RegexMatch.java
@@ -0,0 +1,21 @@
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexMatch {
+    public static boolean containsOnlyAlpha(String s) {
+        return Pattern.matches("[a-zA-Z]+", s);
+    }
+
+    public static boolean startWithLetterAndEndWithNumber(String s) {
+        // s.matches()
+        Pattern alpha = Pattern.compile("^[a-zA-Z].*\\d$");
+        Matcher m = alpha.matcher(s);
+        return m.matches();
+
+    }
+
+    public static boolean containsAtLeast3SuccessiveA(String s) {
+        return !Pattern.matches(".*a{3,}.*", s);
+    }
+}
